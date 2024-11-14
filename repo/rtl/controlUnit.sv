54548ce3e4971e02510e5d0d0869f79c14f1d730
parthak314 <parthakfree@gmail.com>
2024-11-14 10:44:40 +0000

    test commit

---
+++ repo/rtl/controlUnit.sv
@@ -0,0 +1,12 @@
+module controlUnit (
+    input   logic       EQ,
+    input   logic       instr,
+    output  logic       RegWrite,
+    output  logic       ALUctrl,
+    output  logic       ALUsrc,
+    output  logic       ImmSrc,
+    output  logic       PCsrc
+);
+
+
+endmodule
