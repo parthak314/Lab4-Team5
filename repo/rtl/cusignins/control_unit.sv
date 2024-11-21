02d98753eee7293cc4648d266c60b38359d63dd0
Joel <nzmjoel@gmail.com>
2024-11-21 10:38:14 +0000

    added stub integration

--- repo/rtl/cusignins/control_unit.sv
+++ repo/rtl/cusignins/control_unit.sv
@@ -1,11 +1,11 @@
-module controlUnit #(
+module control_unit #(
 ) (
     input logic [6:0]   op,
-    input logic         zero,
-    output logic        PCSrc,
+    input logic         eq,
+    output logic        PCsrc,
     output logic [2:0]  ALUControl,
     output logic        ALUSrc,
-    output logic        IMMSrc,
+    output logic        ImmSrc,
     output logic        RegWrite
 );
     logic branch;
@@ -15,9 +15,9 @@ module controlUnit #(
             7'b0100011: RegWrite = 1'b0; ImmSrc = 2'b1; ALUSrc = 1'b1; branch = 1'b0; ALUControl = 3'b000;
             7'b0110011: RegWrite = 1'b1; ImmSrc = 2'b0; ALUSrc = 1'b0; branch = 1'b0; ALUControl = 3'b000;
             7'b1100011: RegWrite = 1'b0; ImmSrc = 2'b0; ALUSrc = 1'b0; branch = 1'b1; ALUControl = 3'b001;
-            default: RegWrite = 1'b1; ImmSrc = 1'b0; ALUSrc = 1'b1; branch = 1'b0; ALUOp = 3'b000;
+            default: RegWrite = 1'b1; ImmSrc = 1'b0; ALUSrc = 1'b1; branch = 1'b0; op = 3'b000;
         endcase
-    PCSrc = branch && zero
+    PCSrc = branch && eq
     end
 
 endmodule
