f583df1ba16e6a21c6435a40c6e70bc40e11f274
Joel <nzmjoel@gmail.com>
2024-11-21 11:32:01 +0000

    full integration of lab4

--- repo/rtl/cusignins/control_unit.sv
+++ repo/rtl/cusignins/control_unit.sv
@@ -3,19 +3,19 @@ module control_unit #(
     input logic [6:0]   op,
     input logic         eq,
     output logic        PCsrc,
-    output logic [2:0]  ALUControl,
-    output logic        ALUSrc,
+    output logic [2:0]  ALUctrl,
+    output logic        ALUsrc,
     output logic        ImmSrc,
     output logic        RegWrite
 );
     logic branch;
     always_comb begin
         case (op)
-            7'b0000011: RegWrite = 1'b1; ImmSrc = 2'b0; ALUSrc = 1'b1; branch = 1'b0; ALUControl = 3'b000;
-            7'b0100011: RegWrite = 1'b0; ImmSrc = 2'b1; ALUSrc = 1'b1; branch = 1'b0; ALUControl = 3'b000;
-            7'b0110011: RegWrite = 1'b1; ImmSrc = 2'b0; ALUSrc = 1'b0; branch = 1'b0; ALUControl = 3'b000;
-            7'b1100011: RegWrite = 1'b0; ImmSrc = 2'b0; ALUSrc = 1'b0; branch = 1'b1; ALUControl = 3'b001;
-            default: RegWrite = 1'b1; ImmSrc = 1'b0; ALUSrc = 1'b1; branch = 1'b0; op = 3'b000;
+            7'b0000011: RegWrite = 1'b1; ImmSrc = 2'b0; ALUsrc = 1'b1; branch = 1'b0; ALUctrl = 3'b000;
+            7'b0100011: RegWrite = 1'b0; ImmSrc = 2'b1; ALUsrc = 1'b1; branch = 1'b0; ALUctrl = 3'b000;
+            7'b0110011: RegWrite = 1'b1; ImmSrc = 2'b0; ALUsrc = 1'b0; branch = 1'b0; ALUctrl = 3'b000;
+            7'b1100011: RegWrite = 1'b0; ImmSrc = 2'b0; ALUsrc = 1'b0; branch = 1'b1; ALUctrl = 3'b001;
+            default: RegWrite = 1'b1; ImmSrc = 1'b0; ALUsrc = 1'b1; branch = 1'b0; ALUctrl = 3'b000;
         endcase
     PCSrc = branch && eq
     end
--- repo/rtl/top.sv
+++ repo/rtl/top.sv
@@ -6,26 +6,50 @@ module top #(
     output  logic [DATA_WIDTH-1:0] a0    
 );
     assign a0 = 32'd5;
-    logic [DATA_WIDTH-1:0] pc;
-    logic [DATA_WIDTH-1:0] instr;
-    logic eq, RegWrite, AlUctrl, 
+    logic [DATA_WIDTH-1:0] pc, instr, immop;
+    logic eq, pcsrc, alusrc, immsrc, regwrite;
+    logic [2:0] aluctrl;
 
-    pc_top pc_top_inst (
+    pc_top pc_top_mod (
         .clk(clk),
         .rst(rst),
-        .PCsrc(),
-        .ImmOp(),
+        .PCsrc(pcsrc),
+        .ImmOp(immop),
         .PC(pc)
     );
 
-    rom instr_mem (
+    rom instr_mem_mod (
         .addr(pc),
         .instr(instr)
-    )
+    );
 
-    control_unit control (
-        .op(),
-        .eq()
+    control_unit control_mod (
+        .op(instr[6:0]),
+        .eq(eq),
+        .PCsrc(pcsrc),
+        .ALUctrl(aluctrl),
+        .ALUsrc(alusrc),
+        .ImmSrc(immsrc),
+        .RegWrite(regwrite)
+    );
+
+    signext signext_mod (
+        .instr(instr[31:20]),
+        .immSrc(immsrc),
+        .immOp(immop)
+    );
+
+    alu_top alu_mod (
+        .clk(clk),
+        .ALUctrl(aluctrl),
+        .ALUsrc(alusrc),
+        .rs1(instr[19:15]),
+        .rs2(instr[24:20]),
+        .rd(instr[11:7]),
+        .RegWrite(regwrite),
+        .ImmOp(immop),
+        .a0(a0),
+        .EQ(eq)
     )
 
 endmodule
