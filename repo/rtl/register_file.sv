d24683191a3712772ce9a3d9537f437d503753e2
Clarke Chong <149320067+clarkechong@users.noreply.github.com>
2024-11-28 10:59:17 +0000

    edit A0 output to x10 register

--- repo/rtl/register_file.sv
+++ repo/rtl/register_file.sv
@@ -18,12 +18,12 @@ logic[DATA_WIDTH-1:0] reg_file [0:2**ADDRESS_WIDTH-1];
 always_comb begin
     RD1 = reg_file[AD1];
     RD2 = reg_file[AD2];
+    A0 <= reg_file[10];
 end
 
 always @ (posedge clk) begin
     if (WE3) begin
         reg_file[AD3] <= WD3;
-        A0 <= reg_file[AD3];
     end
 end   
 
